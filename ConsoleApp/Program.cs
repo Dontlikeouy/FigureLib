29000b27a3e22d1f4111bb2648487ca6a6389561
Dontlikeouy <86613662+Dontlikeouy@users.noreply.github.com>
2023-11-17 20:51:48 +0300

    Добавил пример использование библиотеки

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using FigureLib;
+
+//Круг
+var circle = new Circle();
+Console.WriteLine($"Площадь круга через радиус - {circle.SquareRadius(10)}");
+Console.WriteLine($"Площадь круга через диаметр - {circle.SquareDiameter(10)}");
+
+
+//Треугольник
+var triangle = new Triangle();
+
+int a = 15, b = 9, c = 12;
+if (triangle.IsRectangle(a, b, c))
+{
+    Console.WriteLine($"Площадь треугольника через три стороны - {triangle.Square(a, b, c)}");
+}
